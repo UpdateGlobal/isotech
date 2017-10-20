4f456d7d0f92c7e8e466fda92c611e5f9482e14b
Frontend Update <arte@update.pe>
2017-10-20 13:14:13 -0500

    formulario de isotech
    funciona ya lo probe en el servidor local envia al correo.
    la pagina de exito falta darle estilos te estoy dando un adelanto todavia.

---
+++ envio.php
@@ -0,0 +1,11 @@
+<?php
+$destino="torralbaalejandro@hotmail.com";
+$nombre= $_POST["nombre"];
+$correo= $_POST["correo"];
+$asunto= $_POST["asunto"];
+$mensaje= $_POST["mensaje"];
+
+$contenido = "Nombre: " . $nombre . "\nCorreo: " . $correo . "\nAsunto: " . $asunto . "\nMensaje: " . $mensaje;
+mail($destino,"Contacto", $contenido);
+header("Location:exito.html");
+?>
